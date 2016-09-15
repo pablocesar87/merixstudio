68f761166ee766d180e36000293323b2836f0d68
pablocesar87 <pablocesar1987@gmail.com>
2016-09-15 14:46:48 +0200

    added test

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -7,15 +9,16 @@ class Entry(models.Model):
     title = models.CharField(max_length=200)
     text= models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True,
+                                         null=True)  # If there is no modification, published date will be shown
     published_date = models.DateTimeField(blank=True, null=True)
-    modified_date = models.DateTimeField(null=True) #If there is no modification, published date will be shown
     comment_count = models.IntegerField(blank=True, default=0)
 
 
-    def publish(self):
-        self.published_date=timezone.now()
-        self.save()
-
     def __str__(self):
         return self.title
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.published_date <= now
 
